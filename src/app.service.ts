4afe3bf48c1d1dfc7a0fcbcffe7a1c513e25f81c
Nourankh20 <82681541+Nourankh20@users.noreply.github.com>
2022-06-08 16:49:22 +0200

    not.

---
+++ src/app.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+const sgMail = require('@sendgrid/mail')
+
+async function sendMail(to:string, subject:string, text:string,  html:string,) {
+  const msg = {
+    to: to, // Change to your recipient
+    from: 'mk.elbaz9248@gmail.com', // Change to your verified sender
+    subject: subject,
+    text: text,
+    html: html
+  }
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode)
+      console.log(response[0].headers)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+
+}
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  orderCreated(to:string, stripeId:string){
+    let text = `Your order has been confirmed! Order no. ${stripeId}.`;
+    sendMail(to,'Order Confirmed!',text,text);
+  }
+  
+}
