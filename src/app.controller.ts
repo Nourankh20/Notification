99df9ffa8a0ceeb887b6af8b4c8d1eedae66ef02
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2022-06-10 12:53:13 +0200

    finalizing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,11 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
-    this.appService.getHello();
+    this.appService.consume()
   }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/notify')
-  sendMail() {
-    return this.appService.orderCreated("nourankh201617@gmail.com", "stripeId",);
-  }
+ 
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,42 +1,91 @@
 import { Injectable } from '@nestjs/common';
 const sgMail = require('@sendgrid/mail')
-require('dotenv').config();
+import { config } from '../config';
+import * as AWS from 'aws-sdk';
+const { Consumer } = require('sqs-consumer');
 
-async function sendMail(to:string, subject:string, text:string,  html:string,) {
-  const msg = {
-    to: to, // Change to your recipient
+
+
+async function sendMail(body : any) {
+  
+  const z = await body.Message
+  console.log(z)
+  const y = await JSON.parse(z);
+  console.log(y.header.event);
+  const f = await y.header.event.name
+  const msg =  {
+    to: y.header.event.email, // Change to your recipient
     from: 'mk.elbaz9248@gmail.com', // Change to your verified sender
-    subject: subject,
-    text: text,
-    html: html
+    subject: `${f} order`,
+    text: `Your order has been confirmed! Order no. ${y.header.event.orderNo}.`,
+    html: `Your order has been confirmed! Order no. ${y.header.event.orderNo}.`,
+    
   }
-
-  sgMail.setApiKey(process.env.sendgrid_api_key);
-  await sgMail
+  console.log(msg);
+  
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  sgMail
     .send(msg)
     .then((response) => {
+      
       console.log(response[0].statusCode)
       console.log(response[0].headers)
-      return response[0].statusCode;
     })
     .catch((error) => {
       console.error(error)
-      return error
     })
-    
 
 }
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  params = {
+    QueueUrl: config.NOTIFICATION_SQS,
+  };
+  private sqs;
+  private sns;
+  constructor() {
+    AWS.config.update({
+      region: 'us-east-1',
+      accessKeyId: config.ACCESS_KEY_ID,
+      secretAccessKey: config.SECRET_ACCESS_KEY,
+    });
+    this.sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+    this.sns = new AWS.SNS({ apiVersion: '2010-03-31' });
+  }
+
+
+  consume() {
+    
+    Consumer.create({
+      queueUrl: config.NOTIFICATION_SQS,
+      handleMessage: async (message) => {
+
+        var params = {
+          Entries: [ /* required */
+            {
+              Id: message.MessageId, /* required */
+              ReceiptHandle: message.ReceiptHandle/* required */
+            },
+            /* more items */
+          ],
+          QueueUrl: config.NOTIFICATION_SQS /* required */
+        };
+
+        const x = message.Body
+        // console.log(x)
+        sendMail(JSON.parse(x))
+        this.sqs.deleteMessageBatch(params, function(err, data) {
+          if (err) console.log(err, err.stack); // an error occurred
+          else     console.log(data);           // successful response
+        });
+      },
+    }).start()
+
   }
 
   orderCreated(to:string, stripeId:string){
     let text = `Your order has been confirmed! Order no. ${stripeId}.`;
-    return sendMail(to,'Order Confirmed!',text,text);
-    // ,process.env.sendgrid_api_key;
-    // return 5;
+    // sendMail(to,'Order Confirmed!',text,text);
   }
   
 }
