75bdb2013343c492354f9ad28fc587b98b5b4814
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2022-06-11 13:49:49 +0200

    trial

--- src/app.service.ts
+++ src/app.service.ts
@@ -60,9 +60,11 @@ export class AppService {
   }
 
    async consume() {
+     console.log("consumer start")
     Consumer.create({
       queueUrl: process.env.NOTIFICATION_SQS_K,
       handleMessage: async (message) => {
+     console.log("handleMessage start")
 
         var params = {
           Entries: [ /* required */
@@ -82,7 +84,7 @@ export class AppService {
         sendMail(y);
         this.sqs.deleteMessageBatch(params, function (err, data) {
           if (err) console.log(err, err.stack); // an error occurred
-          else console.log(data);           // successful response
+          else console.log("deleted ",data);           // successful response
         });
         
       },
